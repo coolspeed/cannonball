9be2acc0a5d158b372e61a6d38127d9653b8dd51
coolspeed <jinxionglong@gmail.com>
2018-12-06 01:30:05 +0900

    Git add missing sdl2_vs.cmake

--- cmake/sdl2_vs.cmake
+++ cmake/sdl2_vs.cmake
@@ -1,17 +1,22 @@
-# CMake setup used for SDL2 Builds.
+# CMake setup used for SDL2 Builds in Visual Studio.
+
+# Set SDL2 instead of SDL1
+set(SDL2 1)
+# SET(CMAKE_FIND_LIBRARY_SUFFIXES ".lib" ".dll")
 
 set(lib_base D:/lib)
-set(sdl_root ${lib_base}/SDL2-2.0.9)
+set(sdl_root ${lib_base}/SDL2)
 
-include_directories("${sdl_root}")
-
-link_libraries(cannonball 
-    SDL2
-)
+include_directories("${sdl_root}/include")
 
 # Linking
 link_directories(
     "${sdl_root}/lib/x64"
+)
+
+link_libraries(
+    cannonball
+    SDL2
 )
 
 add_definitions(-O3 -DSDL2)
@@ -20,6 +25,3 @@ add_definitions(-O3 -DSDL2)
 # Used to auto-generate setup.hpp with various file paths
 set(xml_directory ./)
 set(sdl_flags "SDL_WINDOW_RESIZABLE")
-
-# Set SDL2 instead of SDL1
-set(SDL2 1)
